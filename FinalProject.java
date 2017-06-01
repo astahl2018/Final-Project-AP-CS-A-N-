736d4f72d69da265caff030b01562cf5bfd95284
Nico Espinosa <nico.espinosadice@gmail.com>
2017-06-01 11:50:48 -0400

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -1,32 +1,46 @@
+//Andrew Stahl and Nico Espinosa Dice
+// 06/1/2017
+//Final Project
+//This lab creates an educational game where the user must enter correct responses to questions 
+
 import java.awt.*;
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.io.File;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+//import statements
 
-//do not modify
 public class FinalProject{
+//creates class
+	
 	  private FinalProjectDisplay display;
 	  private int count = 0;
 	  private int situation = 0;
+	  //creates variables
 	  
-	public static void main(String[] args) {
-	    FinalProject lab = new FinalProject(200, 200);
+	public static void main(String[] args) throws FileNotFoundException {
+	//main
+	    FinalProject lab = new FinalProject();
 	    lab.run();
+	    //creates and runs the game
 	  }
 	
-	public FinalProject(int numRows, int numCols) {
+	public FinalProject() throws FileNotFoundException{
+		//constructor creates display
 	    String[] names = {"Andrew", "Nico"};
-	    display = new FinalProjectDisplay("Final Project", numRows, numCols);
+	    display = new FinalProjectDisplay("Final Project");
 	  }
 	
 	
-	public void updateDisplay() {
-		
-	  }
-	
-	public void step() {
-		
-	}
-	
-	public void run() {
+	public void run() throws FileNotFoundException {
 		//runs the program
 	  for (int i = 0; i < 10; i++) {
 		  display.repaint();
